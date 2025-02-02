d024108474eb0699490e3d00bc323b61255fcc95
DJSherwood <sherwood.daniel@gmail.com>
2025-02-01 18:10:39 -0600

    The download data class is now working

--- DownloadTransform.py
+++ DownloadTransform.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import rioxarray as rio
 import rasterio
 from matplotlib.cm import RdYlGn,jet,RdBu
+import matplotlib.pyplot as plt
 
 # Import Planetary Computer tools
 import stackstac
@@ -23,46 +24,6 @@ BOUNDS = (LOWER_LEFT[1], LOWER_LEFT[0], UPPER_RIGHT[1], UPPER_RIGHT[0])
 TIME_WINDOW = "2021-06-01/2021-09-01"
 RESOLUTION = 10
 SCALE = RESOLUTION / 111320.0 # degrees per pixel for crs=4326
-
-# outside method to save xarray to file
-def save_xarray(data_xarray):
-    return  # save x data for intermediate step
-
-# outside method to save xarray to geotiff
-def save_geotiff(saved_data,filename,iselection):
-    data_slice = saved_data.data.isel(time=iselection)
-    # calculate the dimensions of the file
-    height = data_slice.dims["latitude"]
-    width = data_slice.dims["longitude"]
-    # define the CRS and bounding box
-    gt = rasterio.transform.from_bounds(
-        LOWER_LEFT[1],
-        LOWER_LEFT[0],
-        UPPER_RIGHT[1],
-        UPPER_RIGHT[0],
-        width,
-        height
-    )
-    data_slice.rio.write_crs("epsg:4326", inplace=True)
-    data_slice.rio.write_transform(transfrom=gt, inplace=True)
-    # Create the GeoTIFF output
-    with rasterio.open(
-            filename,
-            'w',
-            driver='GTiff',
-            width=width,
-            height=height,
-            crs='epsg:4326',
-            transform=gt,
-            count=4,
-            compress='lzw',
-            dtype='float64'
-    ) as dst:
-        dst.write(data_slice.B01,1)
-        dst.write(data_slice.B04,2)
-        dst.write(data_slice.B06,3)
-        dst.write(data_slice.B08,4)
-        dst.close()
 
 # class to download data from planetary computer
 class DownloadData:
@@ -79,17 +40,17 @@ class DownloadData:
         search = stac.search(
             bbox=BOUNDS,
             datetime=TIME_WINDOW,
-            collections=["sentinel-2-12a"],
+            collections=["sentinel-2-l2a"],
             query={"eo:cloud_cover": {"lt": 30}},
         )
         # number of items used
-        items = list(search.get_items())
-        self.num_items = len(items)
+        returned_items = list(search.get_items())
+        self.num_items = len(returned_items)
         # Load data into xarray
-        signed_items = [planetary_computer.sign(item).to_dict() for item in items]
+        signed_items = [planetary_computer.sign(item).to_dict() for item in returned_items]
         # define resolution for final product
         self.data = stac_load(
-            items,
+            returned_items,
             bands = ["B01","B02","B03","B04","B05","B06","B07","B08","B8A","B11","B12"],
             crs="EPSG:4326",
             resolution=SCALE,
@@ -100,16 +61,32 @@ class DownloadData:
         )
         return
 
+    def save_xarray(self, filename):
+        self.data.to_netcdf(self.filepath + filename)
+        return  # save x array data
+
+class PlotData:
+    def __init__(self,filepath):
+        self.filepath = filepath
+        self.data = None
+
+    def load_xarray(self, filename):
+        self.data = xr.open_dataset(self.filepath + filename)
+        return
+
     def plot_wrap_data(self,selection):
-        plot_data = self.data[[selection]].to_array()
+        plt.figure()
+        plot_data = self.data[selection].to_array()
         # need to save this to an images folder
-        plot_data.plot.imshow(
+        plot_data.plot(
             col='time',
             col_wrap=4,
             robust=True,
             vmin=0,
             vmax=2500
         )
+        plt.savefig(self.filepath + '/some_data.png', dpi=300)
+        plt.close()
         return
 
     def plot_pane_data(self,selection,iselection):
@@ -122,17 +99,19 @@ class DownloadData:
         plt.show()
         return
 
+
 # Class transforms x array data and saves as geotiff
 class TransformData:
     def __init__(self,filepath):
+        self.filepath = filepath
         self.data = None
         self.median = None
         self.ndvi_median = None
         self.ndbi_median = None
         self.ndwi_median = None
 
-    def load_data(self):
-        self.data = ( load data from saved x array )
+    def load_data(self,filename):
+        self.data = xr.DataArray.load(self.filepath + filename)
         return
 
     def transform(self):
@@ -142,17 +121,54 @@ class TransformData:
         self.ndwi_median = (self.median.B03 - self.median.B08)/(self.median.B03 + self.median.B08)
         return
 
+    def save_geotiff(self, filename, iselection):
+        data_slice = self.data.isel(time=iselection)
+        # calculate the dimensions of the file
+        height = data_slice.dims["latitude"]
+        width = data_slice.dims["longitude"]
+        # define the CRS and bounding box
+        gt = rasterio.transform.from_bounds(
+            LOWER_LEFT[1],
+            LOWER_LEFT[0],
+            UPPER_RIGHT[1],
+            UPPER_RIGHT[0],
+            width,
+            height
+        )
+        data_slice.rio.write_crs("epsg:4326", inplace=True)
+        data_slice.rio.write_transform(transfrom=gt, inplace=True)
+        # Create the GeoTIFF output
+        with rasterio.open(
+                filename,
+                'w',
+                driver='GTiff',
+                width=width,
+                height=height,
+                crs='epsg:4326',
+                transform=gt,
+                count=4,
+                compress='lzw',
+                dtype='float64'
+        ) as dst:
+            dst.write(data_slice.B01, 1)
+            dst.write(data_slice.B04, 2)
+            dst.write(data_slice.B06, 3)
+            dst.write(data_slice.B08, 4)
+            dst.close()
+        return
+
 # and then call these functions
 if __name__ == "__main__":
-    dd = DownloadData(filepath='')
-    dd.get_data()
-    dd.plot_wrap_data(selection='')
-    save_xarray(dd.data)
-
-    td = TransformData(filepath='')
-    td.load_data(dd.data)
+    # dd = DownloadData(filepath=r'C:/Users/HG749BX/PycharmProjects/UrbanHeatIndex/')
+    # dd.get_data()
+    # dd.save_xarray('download_01Feb2025.nc')
+    # print(dd.num_items)
+    #
+    td = TransformData(filepath='rC:/Users/HG749BX/PycharmProjects/UrbanHeatIndex/')
+    td.load_data('download_01Feb2025.nc')
     td.transform()
-    save_geotiff(filename=)
+    td.save_geotiff(filename='geotiff_01Feb2025',iselection=7)
+
 
 
 
